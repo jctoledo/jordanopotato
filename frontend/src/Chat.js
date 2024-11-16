36489ecaa8bb037e46d1a0c304566c00b539989f
Jose Cruz Toledo <1273555+jctoledo@users.noreply.github.com>
2024-11-16 12:49:39 -0800

    rebuilt

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
 
 function Chat() {
   const [messages, setMessages] = useState([
